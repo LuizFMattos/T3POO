82ba1ccbb357c90bcadc902223b509a0a1404999
Luiz Filipe Mattos <luizfilipe.mattos96@gmail.com>
2019-12-15 17:19:21 -0300

    Revert "a"
    This reverts commit f66eafb85c407093276b01b4e327ac291a7924ee.

--- T3-QT/BibliotecaDC/bibliotecadc.h
+++ T3-QT/BibliotecaDC/bibliotecadc.h
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 #ifndef BIBLIOTECADC_H
 #define BIBLIOTECADC_H
 
@@ -41,27 +40,3 @@ private:
     Ui::BibliotecaDC *ui;
 };
 #endif // BIBLIOTECADC_H
-=======
-#ifndef BIBLIOTECADC_H
-#define BIBLIOTECADC_H
-#include"Livro.h"
-
-#include <QMainWindow>
-
-QT_BEGIN_NAMESPACE
-namespace Ui { class BibliotecaDC; }
-QT_END_NAMESPACE
-
-class BibliotecaDC : public QMainWindow
-{
-    Q_OBJECT
-
-public:
-    BibliotecaDC(QWidget *parent = nullptr);
-    ~BibliotecaDC();
-
-private:
-    Ui::BibliotecaDC *ui;
-};
-#endif // BIBLIOTECADC_H
->>>>>>> Stashed changes
