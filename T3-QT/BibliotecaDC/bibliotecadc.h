f66eafb85c407093276b01b4e327ac291a7924ee
Fernando Kiyoshi Kaida <fernando.k.kaida@gmail.com>
2019-12-15 12:18:07 -0300

    a

--- T3-QT/BibliotecaDC/bibliotecadc.h
+++ T3-QT/BibliotecaDC/bibliotecadc.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #ifndef BIBLIOTECADC_H
 #define BIBLIOTECADC_H
 
@@ -36,3 +37,27 @@ private:
     Ui::BibliotecaDC *ui;
 };
 #endif // BIBLIOTECADC_H
+=======
+#ifndef BIBLIOTECADC_H
+#define BIBLIOTECADC_H
+#include"Livro.h"
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class BibliotecaDC; }
+QT_END_NAMESPACE
+
+class BibliotecaDC : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    BibliotecaDC(QWidget *parent = nullptr);
+    ~BibliotecaDC();
+
+private:
+    Ui::BibliotecaDC *ui;
+};
+#endif // BIBLIOTECADC_H
+>>>>>>> Stashed changes
--- T3-QT/BibliotecaDC/main.cpp
+++ T3-QT/BibliotecaDC/main.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #include "bibliotecadc.h"
 #include <QApplication>
 
@@ -32,3 +33,35 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
+=======
+#include "bibliotecadc.h"
+#include <QApplication>
+
+#include "DAO.h"
+#include "Livro.h"
+
+#define PESSOA     0
+#define LIVRO      1
+#define EMPRESTIMO 2
+
+int main(int argc, char *argv[])
+{
+    Livro l1(1, "Mochileiro das Galáxias1");
+    Livro l2(2, "Mochileiro das Galáxias2");
+    Livro l3(3, "Mochileiro das Galáxias3");
+    Livro l4(4, "Mochileiro das Galáxias4");
+    Livro l5(5, "Mochileiro das Galáxias5");
+    DAO<Livro> d(LIVRO);
+
+//    d.salvar(l1);
+//    d.salvar(l2);
+//    d.salvar(l3);
+//    d.salvar(l4);
+//    d.salvar(l5);
+
+    QApplication a(argc, argv);
+    BibliotecaDC w;
+    w.show();
+    return a.exec();
+}
+>>>>>>> Stashed changes
